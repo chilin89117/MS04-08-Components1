6dd742d64163b76bd6589d97713efe1a036a3785
Chi Lin <chilin89117@gmail.com>
2017-10-08 23:21:15 -0700

    Up to lecture 108

---
+++ README.md
@@ -0,0 +1,18 @@
+# 04-components (Not with Laravel)
+
+> MS04-04 Components Communication
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{switchName()}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      user_name: {                     // Validation of data passed
+        type: String,
+        required: true,
+        // default: 'Default string',
+      },
+    },
+    methods: {
+      switchName() {
+        return this.user_name.split("").reverse().join("");
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>
